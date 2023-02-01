784d96bb98bcc2a27337312a4a8d610b8043c9f7
Stéphane <sudoweezy@gmail.com>
2023-02-01 11:04:52 +0100

    Last Call + little change

--- assets/arc-0015/application.py
+++ assets/arc-0015/application.py
@@ -1,63 +1,62 @@
 from typing import Final
-from pyteal import *
-from beaker import *
+import beaker as bk
+import pyteal as pt
 from beaker.lib.storage import Mapping
 from typing import Literal
-algod_client = sandbox.get_algod_client()
+algod_client = bk.sandbox.get_algod_client()
 
 # Use a box per member to denote membership parameters
-class SR(abi.NamedTuple):
-    address: abi.Field[abi.Address]
-    round: abi.Field[abi.Uint64]
+class SR(pt.abi.NamedTuple):
+    address: pt.abi.Field[pt.abi.Address]
+    round: pt.abi.Field[pt.abi.Uint64]
 
-class Master(Application):
-    public_key: Final[ApplicationStateValue] = ApplicationStateValue(
-        stack_type=TealType.bytes,
-        default=Bytes(""),
+class Master(bk.Application):
+    public_key: Final[bk.ApplicationStateValue] = bk.ApplicationStateValue(
+        stack_type=pt.TealType.bytes,
+        default=pt.Bytes(""),
         descr="A Public Key use to encrypt the message",
     )
-    arc: Final[ApplicationStateValue] = ApplicationStateValue(
-        stack_type=TealType.bytes,
-        default=Bytes(""),
+    arc: Final[bk.ApplicationStateValue] = bk.ApplicationStateValue(
+        stack_type=pt.TealType.bytes,
+        default=pt.Bytes(""),
         descr="Format use to encrypt the public key",
     )
-    inbox = Mapping(SR, abi.DynamicBytes)
+    inbox = Mapping(SR, pt.abi.DynamicBytes)
 
-    whitelist = Mapping(abi.Address, abi.String)
+    whitelist = Mapping(pt.abi.Address, pt.abi.String)
 
-    @external(authorize=Authorize.only(Global.creator_address()))
-    def set_public_key(self, key_encryption: abi.String, public_key: abi.StaticBytes[Literal[32]]):
-        return Seq(
+    @bk.external(authorize=bk.Authorize.only(pt.Global.creator_address()))
+    def set_public_key(self, key_encryption: pt.abi.String, public_key: pt.abi.StaticBytes[Literal[32]]):
+        return pt.Seq(
             self.public_key.set(public_key.get()),
             self.arc.set(key_encryption.get()),
         )
 
-    @external(authorize=Authorize.only(Global.creator_address()))
-    def authorize(self, address_to_add: abi.Address, info: abi.String):
-        return Seq(
-            (s := abi.Address()).set(address_to_add.get()),
+    @bk.external(authorize=bk.Authorize.only(pt.Global.creator_address()))
+    def authorize(self, address_to_add: pt.abi.Address, info: pt.abi.String):
+        return pt.Seq(
+            (s := pt.abi.Address()).set(address_to_add.get()),
             self.whitelist[s].set(info))  
 
-    @external
-    def write(self, text: abi.DynamicBytes):
-        return Seq(
-            (s := abi.Address()).set(Txn.sender()),
-            Assert(self.whitelist[s].exists()),
-            (r := abi.Uint64()).set(Global.round()),
+    @bk.external
+    def write(self, text: pt.abi.DynamicBytes):
+        return pt.Seq(
+            (s := pt.abi.Address()).set(pt.Txn.sender()),
+            pt.Assert(self.whitelist[s].exists()),
+            (r := pt.abi.Uint64()).set(pt.Global.round()),
             (v := SR()).set(s, r),
             self.inbox[v].set(text))  
 
-    @delete(authorize=Authorize.only(Global.creator_address()))
+    @bk.delete(authorize=bk.Authorize.only(pt.Global.creator_address()))
     def delete(self):
-        return Approve()   
+        return pt.Approve()   
 
-    @external
-    def remove(self, address: abi.Address, round: abi.Uint64):
-        return Seq(
+    @bk.external
+    def remove(self, address: pt.abi.Address, round: pt.abi.Uint64):
+        return pt.Seq(
             (v := SR()).set(address, round),
-            Pop(self.inbox[v].delete())
+            pt.Pop(self.inbox[v].delete())
             )
-
 
 if __name__ == "__main__":
     app = Master()
