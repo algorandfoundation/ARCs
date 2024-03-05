70dd6d366c098fd1d4e7a20c777d7b40f0c36099
Stefano De Angelis <stefanodeangelis91@gmail.com>
2024-03-05 18:38:47 +0100

    byte signing arc

---
+++ assets/arc-0060/types.ts
@@ -0,0 +1,33 @@
+export enum ScopeType {
+    ARBITRARY,
+    LSIG,
+  }
+
+export type StdData = string
+export type Ed25519Pk = string
+
+export interface StdSignData {
+    data: StdData;
+    signers: Ed25519Pk[];
+}
+
+export interface StdSignMetadata {
+    scope: ScopeType;
+    schema: string;
+    message?: string;
+}
+
+export type SignDataFunction = (
+    arbData: StdSignData[],
+    metadata: StdSignMetadata[],
+) => Promise<(string | null)[]>
+
+export interface ARC60SchemaType {
+    ARC60Domain: string;
+    bytes: string;
+}
+
+export enum ApprovalOption {
+    CONFIRM = "Comfirm",
+    REJECT = "Reject",
+}
